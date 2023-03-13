89d05d3c04240abb2bd65c742b4461dd92ec9d60
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-03-13 04:55:13 +0200

    create editThisPlace Route
    create editThisPlace Route to control editing the place data by the owner , the method prevent any one from editing the place data except the owner and also protected the route so no user can access it

--- controllers/Place/placeController.js
+++ controllers/Place/placeController.js
@@ -1,6 +1,15 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-useless-path-segments */
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
 const Place = require('./../../Models/place');
 const catchAsync = require('./../../utils/catchAsync');
+const Owner = require('./../../Models/owner');
+const AppError = require('../../utils/appError');
+const place = require('./../../Models/place');
 
 exports.getAllPlaces = catchAsync(async (req, res, next) => {
   const places = await Place.find();
@@ -11,5 +20,25 @@ exports.getAllPlaces = catchAsync(async (req, res, next) => {
     data: {
       places,
     },
+  });
+});
+
+exports.editThisPlace = catchAsync(async (req, res, next) => {
+  const token = req.headers.authorization.split(' ')[1];
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const currentOwner = await Owner.findById(decoded.id);
+  // eslint-disable-next-line prefer-destructuring
+  const places = currentOwner.places;
+  const reqID = mongoose.Types.ObjectId(req.params.id);
+  const DoesBelong = await places.find((place) => place._id.equals(reqID));
+  if (!DoesBelong) {
+    return next(new AppError('This Place Does not Belong to you', 401));
+  }
+
+  const newPlace = await Place.findByIdAndUpdate(req.params.id, req.body);
+
+  res.status(200).json({
+    status: 'Success',
+    massage: `place ${req.params.id} has been updated`,
   });
 });
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,16 @@
 /* eslint-disable import/no-useless-path-segments */
 const express = require('express');
 const placeController = require('./../controllers/Place/placeController');
+const authOwnerController = require('./../controllers/Owner/authOwnerController');
 
 const router = express.Router();
 
 //GET ALL PLACES ROUTE (USER AND OWNER AND UNREGISTERED USER CAN ACCESS THIS ROUTE )
 router.route('/getAllPlaces').get(placeController.getAllPlaces);
+
+//EDIT THE PLACE THAT THE OWNER IS OPENING NOW
+router
+  .route('/editThisPlace/:id')
+  .post(authOwnerController.protect, placeController.editThisPlace);
 
 module.exports = router;
