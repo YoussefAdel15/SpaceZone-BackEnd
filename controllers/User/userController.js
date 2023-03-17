a6aab5d56f05005a93c4bad84c89b4a89a41f558
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-03-18 00:55:09 +0200

    Update deleteUser
    now delete user is working // tested with postman

--- controllers/User/userController.js
+++ controllers/User/userController.js
@@ -5,7 +5,6 @@ const User = require('./../../Models/user');
 const catchAsync = require('./../../utils/catchAsync');
 const AppError = require('../../utils/appError');
 
-
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -45,19 +44,29 @@ exports.updateUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
-exports.deleteUser= catchAsync(async (req, res, next) => {
-  const token = req.headers.authorization.split(' ')[1];
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  const currentUser = await User.findById(decoded.id);
-  console.log(currentUser)
-  if (currentUser.id ===(req.params.id) ) {
-   const deleteUser = await User.findByIdAndDelete(currentUser.id);
-   res.status(200).json({
-    status: 'Success',
-    message:`User ${req.params.id} has been deleted succesfully`,
-  })
-  }
-  else {
-    return next(new AppError('This account does not belong to you', 401));
+exports.deleteUser = catchAsync(async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+      return next(new AppError('User not found', 404));
+    }
+
+    if (currentUser._id.toString() !== req.params.id) {
+      return next(new AppError('This account does not belong to you', 401));
+    }
+
+    await User.findByIdAndDelete(currentUser._id);
+
+    res.status(200).json({
+      status: 'Success',
+      message: `User ${req.params.id} has been deleted successfully`,
+    });
+  } catch (err) {
+    return next(err);
   }
 });
