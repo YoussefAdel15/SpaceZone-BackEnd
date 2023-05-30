a6a6d9ee11b92e930f0f6ba7bfbdcf2c6e4453d3
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-05-30 10:52:27 +0300

    disable limiter

--- app.js
+++ app.js
@@ -32,13 +32,13 @@ app.use((req, res, next) => {
   next();
 });
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!',
-});
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour!',
+// });
 
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 app.use(mongoSanitize());
 app.use(xss());
