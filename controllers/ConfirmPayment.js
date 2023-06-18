4ce1b462993b3037d750b12beb64ef511bba3d7c
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-06-19 02:57:05 +0300

    payments working and images working

--- controllers/ConfirmPayment.js
+++ controllers/ConfirmPayment.js
@@ -27,7 +27,7 @@ const successHTML = `
     </style>
 </head>
 <body>
-    <img src="./success.png" alt="Success Icon" width="100">
+    <img src="https://github.com/YoussefAdel15/SpaceZone-BackEnd/blob/Youssef/controllers/success.png?raw=true" alt="Success Icon" width="100">
     <h1>Payment Successful</h1>
     <p>Your payment has been successfully processed.</p>
     <p>Thank you for using SpaceZone!</p>
@@ -52,7 +52,7 @@ const failHTML = `
     </style>
 </head>
 <body>
-    <img src="./computer-fail.svg" alt="Fail Icon" width="100">
+    <img src="https://raw.githubusercontent.com/YoussefAdel15/SpaceZone-BackEnd/7d1dafab4cd9378e158b8ba2566de5b800f49fbf/controllers/computer-fail.svg" alt="Fail Icon" width="100">
     <h1>Payment Failed</h1>
     <p>Your payment has been declined.</p>
     <p>Please try again.</p>
@@ -62,74 +62,21 @@ const failHTML = `
 
 exports.successPayment = catchAsync(async (req, res, next) => {
   const { order } = req.query;
-    const voucher = await Voucher.findOne({ orderID: order });
-    const booking = await Booking.findOne({ orderID: order });
-    if (voucher) {
-      voucher.active = true;
-      await voucher.save();
-    } else if (booking && booking.bookingType === 'sharedAreaSeat') {
-      const place = await Place.findById(booking.placeID);
-      const date = new Date(booking.bookingDate);
-      const startTime = booking.bookingHour;
-      const endTime = startTime + booking.bookingHour;
-      const availableSeatsIndex = booking.bookingSeats;
-      if (availableSeatsIndex.length > 0) {
-        // update the seat hours to booked
-        for (let i = 0; i < availableSeatsIndex.length; i++) {
-          place.seats[availableSeatsIndex[i]].days.forEach((e) => {
-            // check if the date is equal to the date he wants to book
-            if (
-              e.date.toISOString().split('T')[0] ===
-              date.toISOString().split('T')[0]
-            ) {
-              // update the hours to booked from the start time to the end time
-              for (let j = startTime; j < endTime; j++) {
-                if (e.hours.array[j] === false) {
-                  e.hours.array[j] = true;
-                } else {
-                  break;
-                }
-              }
-              // mark the document as modified to save the changes
-              place.markModified(`seats.${availableSeatsIndex[i]}.days`); // Mark this part of the document as modified
-            }
-          });
-        }
-        // save the changes to the database
-        await place.save();
-
-        booking.paymentStatus = true;
-        await booking.save();
-      }
-    } else if (booking && booking.bookingType === 'Room') {
-      room.days.forEach((e) => {
-        // check if the date is equal to the date he wants to book
-        if (
-          e.date.toISOString().split('T')[0] ===
-          date.toISOString().split('T')[0]
-        ) {
-          // update the hours to booked from the start time to the end time
-          for (let j = startTime; j < endTime; j++) {
-            if (e.hours.array[j] === false) {
-              e.hours.array[j] = true;
-            } else {
-              break;
-            }
-          }
-          // mark the document as modified to save the changes
-          currentPlace.markModified(`rooms.${booking.bookingRoom}.days`); // Mark this part of the document as modified
-        }
-      });
-      // update the seat hours to booked
-      // save the changes to the database
-      await currentPlace.save();
-
-      booking.paymentStatus = true;
-      await booking.save();
-    } else if (booking && booking.bookingType === 'silentSeat') {
+  const voucher = await Voucher.findOne({ orderID: order });
+  const booking = await Booking.findOne({ orderID: order });
+  if (voucher) {
+    voucher.active = true;
+    await voucher.save();
+  } else if (booking && booking.bookingType === 'sharedAreaSeat') {
+    const place = await Place.findById(booking.placeID);
+    const date = new Date(booking.bookingDate);
+    const startTime = booking.bookingHour;
+    const endTime = startTime + booking.bookingHour;
+    const availableSeatsIndex = booking.bookingSeats;
+    if (availableSeatsIndex.length > 0) {
       // update the seat hours to booked
       for (let i = 0; i < availableSeatsIndex.length; i++) {
-        currentPlace.silentSeats[availableSeatsIndex[i]].days.forEach((e) => {
+        place.seats[availableSeatsIndex[i]].days.forEach((e) => {
           // check if the date is equal to the date he wants to book
           if (
             e.date.toISOString().split('T')[0] ===
@@ -144,23 +91,77 @@ exports.successPayment = catchAsync(async (req, res, next) => {
               }
             }
             // mark the document as modified to save the changes
-            currentPlace.markModified(
-              `silentSeats.${availableSeatsIndex[i]}.days`
-            ); // Mark this part of the document as modified
+            place.markModified(`seats.${availableSeatsIndex[i]}.days`); // Mark this part of the document as modified
           }
         });
       }
       // save the changes to the database
-      await currentPlace.save();
+      await place.save();
 
       booking.paymentStatus = true;
       await booking.save();
-    } else {
-      return next(
-        new AppError(`No voucher or booking found with this order id ${order}`, 400)
-      );
     }
-    res.send(successHTML);
+  } else if (booking && booking.bookingType === 'Room') {
+    room.days.forEach((e) => {
+      // check if the date is equal to the date he wants to book
+      if (
+        e.date.toISOString().split('T')[0] === date.toISOString().split('T')[0]
+      ) {
+        // update the hours to booked from the start time to the end time
+        for (let j = startTime; j < endTime; j++) {
+          if (e.hours.array[j] === false) {
+            e.hours.array[j] = true;
+          } else {
+            break;
+          }
+        }
+        // mark the document as modified to save the changes
+        currentPlace.markModified(`rooms.${booking.bookingRoom}.days`); // Mark this part of the document as modified
+      }
+    });
+    // update the seat hours to booked
+    // save the changes to the database
+    await currentPlace.save();
+
+    booking.paymentStatus = true;
+    await booking.save();
+  } else if (booking && booking.bookingType === 'silentSeat') {
+    // update the seat hours to booked
+    for (let i = 0; i < availableSeatsIndex.length; i++) {
+      currentPlace.silentSeats[availableSeatsIndex[i]].days.forEach((e) => {
+        // check if the date is equal to the date he wants to book
+        if (
+          e.date.toISOString().split('T')[0] ===
+          date.toISOString().split('T')[0]
+        ) {
+          // update the hours to booked from the start time to the end time
+          for (let j = startTime; j < endTime; j++) {
+            if (e.hours.array[j] === false) {
+              e.hours.array[j] = true;
+            } else {
+              break;
+            }
+          }
+          // mark the document as modified to save the changes
+          currentPlace.markModified(
+            `silentSeats.${availableSeatsIndex[i]}.days`
+          ); // Mark this part of the document as modified
+        }
+      });
+    }
+    // save the changes to the database
+    await currentPlace.save();
+
+    booking.paymentStatus = true;
+    await booking.save();
+  } else {
+    res.send(failHTML);
+    res.status(400).json({
+      status: 'fail',
+      message: `No voucher or booking found with this order id: ${order}`,
+    });
+  }
+  res.send(successHTML);
 
   /////////////////////////////
 });
