747ee8cf953931cca388932b6935aa0d02928668
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-06-19 02:22:00 +0300

    Update confirm-route.js

--- routes/confirm-route.js
+++ routes/confirm-route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const ConfirmPayment = require('../controllers/ConfirmPayment');
 const router = express.Router();
 
-router.post('/confirm', ConfirmPayment.successPayment);
+router.get('/confirm', ConfirmPayment.successPayment);
 
 module.exports = router;
