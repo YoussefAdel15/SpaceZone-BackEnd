cb96401fefbc780ad300ec43fc628681cb676d1c
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-06-19 03:17:17 +0300

    Update booking.js

--- Models/booking.js
+++ Models/booking.js
@@ -1,58 +1,69 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-  placeID: { // place that user booked
+  placeID: {
+    // place that user booked
     type: mongoose.Schema.ObjectId,
     ref: 'places',
     select: true,
   },
-  userID: { // user who booked
+  userID: {
+    // user who booked
     type: mongoose.Schema.ObjectId,
     ref: 'users',
     select: true,
   },
-  placeName: { // name of place
+  placeName: {
+    // name of place
     type: String,
     required: true,
   },
-  priceToPay: { // price of booking
+  priceToPay: {
+    // price of booking
     type: Number,
     required: true,
   },
-  bookingDate: { // date of booking
+  bookingDate: {
+    // date of booking
     type: Date,
     required: true,
   },
-  bookingHour: {  // number of hours user want to book
+  bookingHour: {
+    // number of hours user want to book
     type: Number,
     required: true,
   },
-  bookingRoom: {  // Room id user want to book
+  bookingRoom: {
+    // Room id user want to book
     type: Number,
     required: false,
   },
-  bookingSeats: { // Seats id user want to book
+  bookingSeats: {
+    // Seats id user want to book
     type: [
       {
         type: Number,
-      }
+      },
     ],
     required: false,
   },
-  bookingStatus: {  // true if booking is confirmed by owner , false if owner didn't confirm
+  bookingStatus: {
+    // true if booking is confirmed by owner , false if owner didn't confirm
     type: Boolean,
     required: true,
   },
-  paymentStatus: {  // true if payment is done , false if payment is not done
+  paymentStatus: {
+    // true if payment is done , false if payment is not done
     type: Boolean,
-    default: false,
     required: true,
   },
-  paymentMethod: { // {Cash , Credit Card , Wallet}
+  paymentMethod: {
+    // {Cash , Credit Card , Wallet}
     type: String,
     required: true,
   },
-  productID: {  // in the future
+  productID: {
+    // in the future
     type: [
       {
         type: mongoose.Schema.ObjectId,
@@ -61,21 +72,24 @@ const bookingSchema = new mongoose.Schema({
       },
     ],
   },
-  startTime: {  // start time of booking
+  startTime: {
+    // start time of booking
     type: Number,
     required: true,
   },
-  endTime: {  // end time of booking
+  endTime: {
+    // end time of booking
     type: Number,
     required: false,
   },
-  bookingType: {  // {Seat , Room}
+  bookingType: {
+    // {Seat , Room}
     type: String,
-    enum: ['silentSeat', 'Room' , 'sharedAreaSeat'],
+    enum: ['silentSeat', 'Room', 'sharedAreaSeat'],
     required: true,
   },
-  orderID :{
+  orderID: {
     type: String,
-  }
+  },
 });
 module.exports = booking = mongoose.model('booking', bookingSchema);
